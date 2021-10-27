f39a5dcb24f6db65781188681fe883019920b433
dogkansaracedu <dogkansarac@sabanciuniv.edu>
2021-10-27 17:59:12 +0300

    First Commit

---
+++ components/main-page/CarouselSlider.vue
@@ -0,0 +1,31 @@
+
+#Carousels
+The v-carousel component is used to display large numbers of visual content on a rotating timer.
+
+#Usage
+The v-carousel component expands upon v-window by providing additional features targeted at displaying images.
+
+<template>
+  <v-carousel v-model="model">
+    <v-carousel-item
+      v-for="(color, i) in colors"
+      :key="color"
+    >
+      <v-sheet
+        :color="color"
+        height="100%"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="text-h2">
+            Slide {{ i + 1 }}
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -5,19 +5,46 @@
         {{ example }}
       </v-col>
     </v-row>
+         <v-col>
+          <v-carousel v-model="model">
+      <v-carousel-item
+        v-for="(color, i) in colors"
+        :key="color"
+      >
+        <v-sheet
+          :color="color"
+          height="100%"
+          tile
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <div class="text-h2">
+              Slide {{ i + 1 }}
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+      </v-col>
   </v-container>
 </template>
 
 <script>
 import axios from "axios"
+
 export default {
   data() {
     return {
       examples: [],
+      colors: ["red", "green", "blue"],
     };
   },
   mounted() {
     this.$api("getFeatured").then(({ data }) => { this.examples = data.map((e) => e.firstName + e.lastName) })
   },
+ 
 };
 </script>
