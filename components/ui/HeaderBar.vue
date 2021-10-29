3b56371ac294e2583085003bfdc89b1a55efe5dc
Ahmet Berk TAS <ahberk.tas@gmail.com>
2021-10-29 15:54:17 +0300

    HeaderBar done

--- components/ui/HeaderBar.vue
+++ components/ui/HeaderBar.vue
@@ -7,33 +7,94 @@
       paddingRight: paddingX,
     }"
   >
-    <div style="width: 100%" class="d-flex align-center">
-      <img src="/logos/header-logo.jpg" height="40" />
+    <v-app-bar-nav-icon v-if="$vuetify.breakpoint.smAndDown" />
+    <div
+      v-if="$vuetify.breakpoint.mdAndUp"
+      style="width: 100%"
+      class="d-flex align-center"
+    >
+      <img src="/logos/header-logo.png" height="40" />
       <h2 class="logo-font primary--text mr-4">{{ $i18n("organicity") }}</h2>
       <v-text-field
-        outlined
+        v-model="searchString"
+        @focus="focusedSearch = true"
+        @blur="focusedSearch = false"
+        solo
+        flat
+        :background-color="focusedSearch ? '#dfdfdf' : '#eee'"
         color="primary"
         dense
-        class="search-field rounded-lg"
+        height="45"
+        class="search-field rounded-lg rounded-r-0"
         clearable
         hide-details
         prepend-inner-icon="search"
+      />
+      <v-btn
+        large
+        depressed
+        :disabled="!focusedSearch && !searchString"
+        class="rounded-l-0 primary"
       >
-        deneme
-      </v-text-field>
-      <v-btn color="primary" text large>Giriş Yap</v-btn>
-      <v-btn color="primary" text large>Üye Ol</v-btn>
+        Ara
+      </v-btn>
+      <v-btn color="primary" text large>{{ $i18n("sign_in") }}</v-btn>
+      <v-btn color="primary" text large>{{ $i18n("sign_up") }}</v-btn>
       <v-btn color="primary" icon large>
         <v-icon>shopping_cart</v-icon>
       </v-btn>
+      <AccessabilityMenu />
     </div>
+    <div
+      v-else
+      style="width: 100%"
+      class="d-flex align-center justify-space-between"
+    >
+      <img src="/logos/header-logo.png" height="30" />
+      <h3 class="logo-font primary--text mr-4">{{ $i18n("organicity") }}</h3>
+      <div class="d-flex">
+        <v-btn color="primary" icon large>
+          <v-icon>shopping_cart</v-icon>
+        </v-btn>
+        <AccessabilityMenu />
+      </div>
+    </div>
+    <template v-if="$vuetify.breakpoint.smAndDown" slot="extension">
+      <div class="d-flex mb-2" style="width: 100%">
+        <v-text-field
+          v-model="searchString"
+          @focus="focusedSearch = true"
+          @blur="focusedSearch = false"
+          solo
+          flat
+          :background-color="focusedSearch ? '#dfdfdf' : '#eee'"
+          color="primary"
+          dense
+          height="45"
+          class="search-field rounded-lg rounded-r-0"
+          clearable
+          hide-details
+          prepend-inner-icon="search"
+        />
+        <v-btn
+          large
+          depressed
+          :disabled="!focusedSearch && !searchString"
+          class="rounded-l-0 primary"
+        >
+          Ara
+        </v-btn>
+      </div>
+    </template>
   </v-app-bar>
 </template>
 
 <script>
+import AccessabilityMenu from './AccessabilityMenu.vue';
 import DarkThemeSwitch from "./DarkThemeSwitch.vue";
+import LanguageSelector from './LanguageSelector.vue';
 export default {
-  components: { DarkThemeSwitch },
+  components: { DarkThemeSwitch, LanguageSelector, AccessabilityMenu },
   computed: {
     paddingX() {
       let screenWidth = this.$vuetify.breakpoint.width
@@ -41,6 +102,12 @@ export default {
       else if (screenWidth < 1264) return "calc((100% - 900px) / 2)"
       else if (screenWidth < 1904) return "calc((100% - 1185px) / 2)"
       else return "calc((100% - 1785px) / 2)"
+    }
+  },
+  data() {
+    return {
+      focusedSearch: false,
+      searchString: ""
     }
   }
 };
