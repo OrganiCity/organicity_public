796ec864dd0c6c7b7e6f2b55ee3cc069dd098197
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-10-30 01:04:17 +0300

    Arrows on Hover for Carousel Slider

--- components/main-page/CarouselSlider.vue
+++ components/main-page/CarouselSlider.vue
@@ -7,13 +7,13 @@ The v-carousel component expands upon v-window by providing additional features 
 
 <template>
 <v-card elevation="0">
-  <v-carousel cycle height="auto" v-model="model"> <!--  You can control carousel with model. + - -->
+  <v-carousel cycle height="auto" v-model="model" show-arrows-on-hover> <!--  You can control carousel with model. + - -->
     <v-carousel-item
       v-for="(item,i) in items"
       :key="i"
       :src = item.src
       :to="item.route"
-      >
+    >
     </v-carousel-item>
   </v-carousel>
 
