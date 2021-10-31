779a2b23fb40d2e2ee6edfe05a44bcbd891ba9c3
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-10-31 23:08:57 +0300

    Carousel-Slider delimiter changed

--- components/main-page/CarouselSlider.vue
+++ components/main-page/CarouselSlider.vue
@@ -1,25 +1,21 @@
-
-#Carousels
-The v-carousel component is used to display large numbers of visual content on a rotating timer.
-
-#Usage
-The v-carousel component expands upon v-window by providing additional features targeted at displaying images.
-
 <template>
-<v-card elevation="0">
-  <v-carousel cycle height="auto" v-model="model" show-arrows-on-hover> <!--  You can control carousel with model. + - -->
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src = item.src
-      :to="item.route"
+  <v-card elevation="0">
+    <v-carousel
+      cycle
+      class="featured"
+      height="auto"
+      hide-delimiter-background
+      show-arrows-on-hover
     >
-    </v-carousel-item>
-  </v-carousel>
-
-
-</v-card>
-    
+      <!--  You can control carousel with model. + - -->
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        :to="item.route"
+      ></v-carousel-item>
+    </v-carousel>
+  </v-card>
 </template>
 
 
@@ -30,8 +26,33 @@ export default {
       items: [],
     };
   },
-   mounted() {
-    this.$api("getFeaturedImages").then(({ data }) => { this.items = data })
-  },  
+  mounted() {
+    this.$api("getFeaturedImages").then(({ data }) => {
+      this.items = data;
+    });
+  },
 };
 </script>
+
+<style scoped>
+.featured >>> .v-carousel_controls_item {
+  color: white !important;
+  background: none !important;
+}
+.featured >>> .v-carousel__controls__item .v-icon {
+  opacity: 1 !important;
+}
+.featured >>> .v-btn--active {
+  color: #2dbd9c !important;
+  border: 3px solid white !important;
+  background-color: #2dbd9c !important;
+}
+
+.featured >>> .v-btn--icon.v-size--small {
+  height: 20px !important;
+  width: 20px !important;
+}
+.featured >>> .v-carousel__controls__item .v-btn__content .v-icon {
+  font-size: 14px !important;
+}
+</style>
