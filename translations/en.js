d49ac91a6bab711984c0c17cccece84bbafb26f9
Ali Tarik <alitarik@sabanciuniv.edu>
2021-11-01 15:31:15 +0300

    added translations for footer

--- translations/en.js
+++ translations/en.js
@@ -3,5 +3,9 @@ export default {
     sign_in: 'Log In',
     sign_up: 'Register',
     close: 'Close',
-    organicity: 'OrganiCity'
+    organicity: 'OrganiCity',
+    home : 'Home',
+    about_us : 'About Us',
+    team : 'Team',
+    contact_us : 'Contact Us'
 }
