6328cd93cd32fe68d1662e74990886122fa7adf8
Ali Tarik <alitarik@sabanciuniv.edu>
2021-11-01 15:31:15 +0300

    added translations for footer

--- translations/tr.js
+++ translations/tr.js
@@ -3,5 +3,9 @@ export default {
     sign_in: 'Giriş Yap',
     sign_up: 'Üye Ol',
     close: 'Kapat',
-    organicity: 'OrganiKent'
+    organicity: 'OrganiKent',
+    home : 'Ana Sayfa',
+    about_us : 'Hakkımızda',
+    team : 'Ekibimiz',
+    contact_us : 'Bize Ulaşın'
 }
