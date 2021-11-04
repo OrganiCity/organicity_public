83ac0dda82ce05a64641bc3c21ebb9f563338860
Ali Tarik <alitarik@sabanciuniv.edu>
2021-11-04 16:06:18 +0300

    added social media icons

--- components/ui/Footer.vue
+++ components/ui/Footer.vue
@@ -14,13 +14,28 @@
       >
         {{ link.name }}
       </v-btn>
-      <v-col
-        class="py-4 text-center"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>{{$i18n("organicity")}}</strong>
-      </v-col>
     </v-row>
+
+    <v-col
+      class="py-1 text-center"
+      cols="12">
+      <v-btn
+          v-for="icon in icons"
+          :key="icon.route"
+          class="mx-4"
+          :href="icon.route"
+          target="_blank"
+          icon>
+        <v-icon size="24px"> {{ icon.icon }} </v-icon>
+      </v-btn>
+    </v-col>
+
+      <v-col
+        class="py-2 text-center"
+        cols="12">
+        {{ new Date().getFullYear() }} — <strong>{{$i18n("organicity")}}</strong>
+        
+      </v-col>
   </v-footer>
 </template>
 
@@ -33,7 +48,12 @@
         {name: this.$i18n("about_us"), route: "/about-us"},
         {name: this.$i18n("team"), route: "/team"},
         {name: this.$i18n("contact_us"), route: "/contact-us"},
-      ]
+      ],
+      icons: [
+        {icon: 'mdi-facebook', route: "https://facebook.com/"},
+        {icon: 'mdi-twitter', route: "https://twitter.com/"},
+        {icon: 'mdi-instagram', route: "https://instagram.com/"}
+      ],
     }
   }
 }
