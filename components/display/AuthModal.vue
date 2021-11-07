8f9028eb3301e162ed55077765c7b326bbcd49d3
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-11-07 23:29:50 +0300

    AuthModal and LogIn Tab added

---
+++ components/display/AuthModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-dialog :max-width="modalMaxWidth" :value="value" @input="$emit('input')">
+    <v-tabs :value="openTab" @change="$emit('tab-change', $event)" centered>
+      <v-tab>{{$i18n("sign_in")}}</v-tab>
+      <v-tab-item>
+        <LoginTab @close="$emit('input')" />
+      </v-tab-item>
+      <v-tab>{{$i18n("sign_up")}}</v-tab>
+      <v-tab-item>
+        <RegisterTab @close="$emit('input')" />
+      </v-tab-item>
+    </v-tabs>
+  </v-dialog>
+</template>
+
+<script>
+import LoginTab from "./LoginTab.vue";
+import RegisterTab from "./RegisterTab.vue";
+export default {
+  components: { RegisterTab, LoginTab },
+  props: ["value", "open-tab"],
+  computed: {
+    modalMaxWidth() {
+      let screen = this.$vuetify.breakpoint.name;
+      switch (screen) {
+        case "xs":
+          return "100%";
+        case "sm":
+          return "50%";
+        default:
+          return "500px";
+      }
+    }
+  }
+};
+</script>
