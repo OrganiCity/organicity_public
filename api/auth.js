299c2a486deadbaa4d3f8a8647caa546fbe4f449
Ahmet Berk TAS <ahberk.tas@gmail.com>
2021-11-08 16:58:40 +0300

    implemented rest apis to the front end for login

---
+++ api/auth.js
@@ -0,0 +1,16 @@
+export async function userLogin({ instance }, { email, password }) {
+    return await instance.post("/auth/login", { email: email, password: password })
+}
+
+export async function userMe({ instance }) {
+    return await instance.get("/auth/me")
+}
+
+export async function userRegister({ instance }, { email, password, firstName, lastName }) {
+    return await instance.post("/auth/login", {
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    })
+}
