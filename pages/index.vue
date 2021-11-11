bb8410009f453b900ab3912120dffe582f3bd519
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-11-11 17:27:12 +0300

    SpecialDeals component size adjusted

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-row justify-content="left">
       <v-col lg="3" md="4" v-if="$vuetify.breakpoint.mdAndUp">
         <CategoryBar/>
-        <SpecialDeals class="pa-10" v-if="$vuetify.breakpoint.md"/>
+        <SpecialDeals class="mx-8 my-8" v-if="$vuetify.breakpoint.md"/>
       </v-col>
       <v-col lg="7" md="8" xs="12">
         <CarouselSlider/>
