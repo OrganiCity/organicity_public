d14b65b92660b19fe3ffa08618c853179395456a
Ahmet Berk TAS <ahberk.tas@gmail.com>
2021-11-15 15:43:30 +0300

    added product layout

---
+++ layouts/product.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app :lang="$store.getters['preferences/language']" class="px-0">
+    <ScrollToTop />
+    <HeaderBar @openSideBar="sideBarActive = true" />
+    <MobileSidebar v-if="$vuetify.breakpoint.xsOnly" v-model="sideBarActive" />
+    <v-main>
+      <v-container fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import DarkThemeSwitch from "~/components/ui/DarkThemeSwitch.vue";
+import Footer from "~/components/ui/Footer.vue";
+import HeaderBar from "~/components/ui/HeaderBar.vue";
+import LanguageSelector from "~/components/ui/LanguageSelector.vue";
+import MobileSidebar from '~/components/ui/MobileSidebar.vue';
+import ScrollToTop from "~/components/ui/ScrollToTop.vue";
+export default {
+  components: {
+    DarkThemeSwitch,
+    LanguageSelector,
+    HeaderBar,
+    Footer,
+    ScrollToTop,
+    MobileSidebar,
+  },
+  middleware: "preferences",
+  head() { return { titleTemplate: `${this.$i18n("organicity")}` } },
+  data() {
+    return {
+      sideBarActive: false,
+    }
+  },
+};
+</script>
