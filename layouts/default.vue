762e51a8fafc6cd9f04f923586eb06feb0701ed3
Ahmet Berk TAS <ahberk.tas@gmail.com>
2021-11-15 15:21:33 +0300

    added sidebar and made lots of changes to the layout of the app

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,16 @@
 <template>
   <v-app :lang="$store.getters['preferences/language']" class="px-0">
     <ScrollToTop />
-    <HeaderBar />
+    <HeaderBar @openSideBar="sideBarActive = true" />
+    <MobileSidebar v-if="$vuetify.breakpoint.xsOnly" v-model="sideBarActive" />
     <v-main>
-      <v-container fluid>
+      <v-container class="d-flex">
+        <div>
+          <CategoryBar v-if="$vuetify.breakpoint.smAndUp" />
+          <SpecialDeals v-if="$vuetify.breakpoint.md" />
+        </div>
         <Nuxt />
+        <SpecialDeals v-if="$vuetify.breakpoint.lgAndUp" />
       </v-container>
     </v-main>
     <Footer />
@@ -12,10 +18,13 @@
 </template>
 
 <script>
+import SpecialDeals from '~/components/main-page/SpecialDeals.vue';
+import CategoryBar from '~/components/ui/CategoryBar.vue';
 import DarkThemeSwitch from "~/components/ui/DarkThemeSwitch.vue";
 import Footer from "~/components/ui/Footer.vue";
 import HeaderBar from "~/components/ui/HeaderBar.vue";
 import LanguageSelector from "~/components/ui/LanguageSelector.vue";
+import MobileSidebar from '~/components/ui/MobileSidebar.vue';
 import ScrollToTop from "~/components/ui/ScrollToTop.vue";
 export default {
   components: {
@@ -24,8 +33,16 @@ export default {
     HeaderBar,
     Footer,
     ScrollToTop,
+    MobileSidebar,
+    CategoryBar,
+    SpecialDeals,
   },
   middleware: "preferences",
-  head() { return { titleTemplate: `${this.$i18n("organicity")}` } }
+  head() { return { titleTemplate: `${this.$i18n("organicity")}` } },
+  data() {
+    return {
+      sideBarActive: false,
+    }
+  },
 };
 </script>
