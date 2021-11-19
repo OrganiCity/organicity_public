c77e6d060230ca0289437a1f64a957131db3e385
Ali Tarik <alitarik@sabanciuniv.edu>
2021-11-19 22:58:44 +0300

    a

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -10,6 +10,7 @@
       <p>Orginity accepts the following certificates: ISO 9001 (Kalite Yönetim Sistemi), ISO 22000 (Gıda Güvenliği Yönetim Sistemi), ISO 14001 (Çevre Yönetim Sistemi), TS EN ISO 9001 Kalite Yönetim Sistemi, TS EN ISO 22000 Gıda Güvenliği Yönetim Sistemi, TSE Helal Uygunluk Belgesi, TS ISO 10002 Müşteri Memnuniyeti Yönetim Sistemi, TS ISO 45001 İş Sağlığı ve Güvenliği Yönetim Sistemi, TS EN ISO 14001 Çevre Yönetim  Sistemi, TS EN ISO 14064 Sera Gazı Hesaplama ve Doğrulama Yönetim Sistemi</p>
       <p>You can reach us from the "Contact Us" page with filling the form or by calling 444 4 444. We evaluate your comments and suggestions as soon as possible and get back to you.</p>
     </v-row>
+    
   </v-container>
 </template>
 
