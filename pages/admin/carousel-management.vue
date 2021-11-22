175774145402a43d20ae5e0f776042043b22429d
Ali Tarik <alitarik@sabanciuniv.edu>
2021-11-22 15:16:12 +0300

    added carousel management

---
+++ pages/admin/carousel-management.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <h1 class="primary--text">Special Deals Management</h1>
+    <SlideManager get-slides-function="getFeaturedImages"></SlideManager>
+  </v-container>
+</template>
+
+<script>
+import SlideManager from '~/components/admin/SlideManager.vue'
+export default {
+  layout: "admin",
+  components: { SlideManager },
+}
+</script>
+
+<style>
+
+</style>
