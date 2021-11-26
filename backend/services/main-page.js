75276c7888ddd2da958e2443bdd3c3d302c4efd1
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-11-26 15:54:20 +0300

    Category connected to database. Breadcrumbs updated accordingly.

--- backend/services/main-page.js
+++ backend/services/main-page.js
@@ -7,3 +7,54 @@ export function getFeatured(req, res) {
         res.json(data)
     })
 }
+
+export function getCategories(req, res) {
+    pool.query(`SELECT node.categoryID, node.name, (COUNT(parent.name) - (sub_tree.depth + 1)) AS depth, node.iconTag
+    FROM nested_category AS node,
+         nested_category AS parent,
+         nested_category AS sub_parent,
+         (
+            SELECT node.name, (COUNT(parent.name) - 1) AS depth
+            FROM nested_category AS node,
+                 nested_category AS parent
+            WHERE node.lft BETWEEN parent.lft AND parent.rgt
+                 AND node.categoryID = 0
+                 GROUP BY node.name
+                 ORDER BY node.lft
+          )AS sub_tree
+    WHERE node.lft BETWEEN parent.lft AND parent.rgt
+          AND node.lft BETWEEN sub_parent.lft AND sub_parent.rgt
+          AND sub_parent.name = sub_tree.name
+    GROUP BY node.name
+    ORDER BY node.lft`,
+        (err, list) => {
+            list.shift()
+            if (err) throw err
+            var map = {}, node, roots = [], i, firstID, secondID;
+
+            for (i = 0; i < list.length; i += 1) {
+                map[list[i].categoryID] = i; // initialize the map
+                list[i].children = []; // initialize the children
+            }
+
+
+            for (i = 0; i < list.length; i += 1) {
+                node = list[i];
+                if (node.depth == 2) {
+                    secondID = node.categoryID
+                    list[map[firstID]].children.push(node);
+                }
+                else if (node.depth == 3) {
+                    list[map[secondID]].children.push(node);
+                }
+
+                else {
+                    firstID = node.categoryID;
+                    roots.push(node);
+                }
+            }
+
+            res.status(200)
+            res.json(roots)
+        })
+}
