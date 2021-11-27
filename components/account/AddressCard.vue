170e3fcff6a620aeebb0f99ddfd6339d28a48c93
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-11-27 22:25:50 +0300

    My Addresses tab added

---
+++ components/account/AddressCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container class="pa-0 ma-0">
+    <!-- Address Preview -->
+
+    <v-card outlined class="mt-6">
+      <v-card-title class="d-flex justify-space-between text-body-1 font-weight-bold pa-2 ma-0">
+        <span class="ma-2">ev</span>
+        <div>
+          <v-btn @click.stop="dialog = true" small class="pa-0 ma-0" icon>
+            <v-icon size="20px">mdi-pencil-outline</v-icon>
+          </v-btn>
+          <v-btn small class="pa-0 ma-0" icon>
+            <v-icon size="20px">delete_outline</v-icon>
+          </v-btn>
+        </div>
+      </v-card-title>
+      <p class="text-caption mx-4">
+        <span>{{ address.ZIPorNeighbourhood }}</span>
+        <span>{{ address.street }}</span>
+        <span>Bina ve Daire No: {{ address.buildingAndFlatNo }}</span>
+        <span>{{ address.district }}</span>
+        <span>/{{ address.city }}/</span>
+        <span>{{ address.country }}</span>
+      </p>
+    </v-card>
+
+    <!-- Address Dialog Menu -->
+    <v-dialog v-model="dialog" overlay-color="secondary" max-width="700px">
+      <AddressDialogMenu :address="address" />
+    </v-dialog>
+  </v-container>
+</template>
+
+<script>
+import AddressDialogMenu from "~/components/account/AddressDialogMenu.vue";
+export default {
+  data: () => ({
+    dialog: false,
+  }),
+  props: {
+    address: {},
+  },
+  components: {
+    AddressDialogMenu,
+  },
+};
+</script>
+
+<style>
+</style>
--- pages/account/my-favorites.vue
+++ pages/account/my-favorites.vue
@@ -1,5 +1,10 @@
 <template>
-  
+   <v-row class="my-4">
+         <v-col cols="12" class="d-flex justify-center">
+      <p class="text-h4 pa-0">Favorilerim</p>
+    </v-col>
+       <v-col v-for="i in 5" :key="i" cols="3" sm="3"><ProductPreview /></v-col>
+   </v-row>
 </template>
 
 <script>
