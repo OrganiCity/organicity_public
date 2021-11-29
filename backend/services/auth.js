9b593ace759d4867a047ef85899084665b4fb014
Ahmet Berk TAS <ahberk.tas@gmail.com>
2021-11-29 16:56:32 +0300

    account update done

--- backend/services/auth.js
+++ backend/services/auth.js
@@ -69,13 +69,3 @@ export function meUser(req, res) {
         return res.status(200).send(decoded);
     });
 }
-
-export function refreshToken(req, res) {
-    var token = req.headers.authorization;
-    if (!(token && token.split(' ')[0] == 'Bearer')) return res.status(401).send('No token provided.');
-    token = token.split(' ')[1]
-    jwt.verify(token, secrets.jwt_secret, function (err, decoded) {
-        if (err) return res.status(401).send('Failed to authenticate token.');
-    
-    });
-}
