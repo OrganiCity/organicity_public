bb2c191ff6773c7b16d62050c59b5fb320c4ab97
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-11-30 00:43:56 +0300

    My Favorites page finished

--- backend/services/account.js
+++ backend/services/account.js
@@ -3,7 +3,7 @@ import { isProvided } from "../utils";
 
 export function updatePersonalInfo(req, res) {
     if (!isProvided(req.body.firstName, req.body.lastName, req.body.gender, req.body.userID))
-    return res.status(400).send();
+        return res.status(400).send();
     const queryText = `
     UPDATE organicity.users
     SET 
@@ -13,7 +13,7 @@ export function updatePersonalInfo(req, res) {
     WHERE userID= ?;
     `;
     let queryValues = [req.body.firstName, req.body.lastName, req.body.gender, req.body.userID];
-    
+
     pool.query(queryText, queryValues, (err, data) => {
         if (err) return res.status(500).send("Internal Server Error")
         return res.status(200).send()
@@ -32,8 +32,22 @@ export function updateContactInfo(req, res) {
             `;
     let queryValues = [req.body.phoneNumber, req.body.email, req.body.userID];
 
-    pool.query(queryText, queryValues, (err, data) => {
+    pool.query(queryText, queryValues, (err) => {
         if (err) return res.status(500).send("Internal Server Error")
         return res.status(200).send()
     })
+}
+
+export function getFavoriteProducts(req, res) {
+    if (!isProvided(req.body.userID)) return res.status(400).send("Id not defined");
+    pool.query(
+        ` SELECT f.productID
+        FROM favorites f
+        WHERE f.userID = ?
+        `,
+        [req.body.userID], (err, data) => {
+            if (err) return res.status(500).send("Internal Server Error")
+            return res.status(200).send(data)
+        }
+    )
 }
