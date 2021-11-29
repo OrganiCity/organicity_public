74566d9b36de3c644df5c41bb3ff567ed7df2485
Ali Tarik <alitarik@sabanciuniv.edu>
2021-11-29 15:07:27 +0300

    added link to special deals

--- components/main-page/SpecialDeals.vue
+++ components/main-page/SpecialDeals.vue
@@ -19,7 +19,7 @@
         height="auto"
         :show-arrows="false"
       >
-        <v-carousel-item v-for="(item, i) in items" :key="i" to="product-page">
+        <v-carousel-item v-for="(item, i) in items" :key="i" :to="'/product/'+item.productID">
           <v-img :aspect-ratio="1" :src="item.src"></v-img>
           <p class="font-weight-medium mt-2" style="text-align: center">
             {{item.productName}}
