2d720905f83c72eaaa9e1e8b66f002f61285e7ab
Ahmet Berk TAS <ahberk.tas@gmail.com>
2021-11-29 15:41:07 +0300

    finished my-cart page

--- components/cart/CartProductPreview.vue
+++ components/cart/CartProductPreview.vue
@@ -1,55 +1,47 @@
 <template>
   <v-card elevation="0" class="pa-4">
     <v-row class="d-flex align-center">
-        <v-col md="2" cols="6">
-            <v-img
-              class="rounded"
-              min-height="80px"
-              max-height="98px"
-              max-width="100px"
-              aspect-ratio="1.4"
-              src="https://www.greenada.com/Uploads/UrunResimleri/buyuk/greenadadomates-1-kg-e162.jpg"
-            ></v-img>
-        </v-col>
-        <v-col md="3" cols="6">
-            <p class="mb-0 d-flex">Organik Domates Kg</p>
-            <p class="text-caption primary--text mb-0">Bizim Çiftlik</p>
-        </v-col>
-        <v-col md="3" cols="5" >
-            <div
-              style="border: 1px solid #2dbd9c; border-radius: 4px"
-              v-if="itemsInBasket"
-              class="d-flex justify-space-between align-center"
-            >
-              <div>
-                <v-btn class="rounded-r-0 secondary" elevation="0" height="30" @click="itemsInBasket--" x-small>
-                  <v-icon color="primary" size="20">{{ itemsInBasket == 1 ? "delete_outline" : "remove" }}</v-icon>
-                </v-btn>
-              </div>
-              <span>{{ itemsInBasket }}</span>
-              <div>
-                <v-btn class="rounded-l-0 secondary" elevation="0" height="30" @click="itemsInBasket++" x-small>
-                  <v-icon color="primary" size="20">add</v-icon>
-                </v-btn>
-              </div>
-            </div>
-        </v-col>
-        <v-col md="2" cols="3" class="d-flex justify-end">
-            <p class="primary--text mb-0 text-body-1 font-weight-medium">75,00 TL</p>
-        </v-col>
-        <v-col md="2" cols="4" class="d-flex justify-end">
-            <p class="mr-4 my-0">Sil</p>
-        </v-col>
+      <v-col md="2" cols="6">
+        <v-img
+          class="rounded"
+          min-height="80px"
+          max-height="98px"
+          max-width="100px"
+          aspect-ratio="1.4"
+          src="https://www.greenada.com/Uploads/UrunResimleri/buyuk/greenadadomates-1-kg-e162.jpg"
+        ></v-img>
+      </v-col>
+      <v-col md="3" cols="6">
+        <p class="mb-0 d-flex">{{ productInfo.productName }}</p>
+        <p class="text-caption primary--text mb-0">{{ productInfo.companyName }}</p>
+      </v-col>
+      <v-col md="3" cols="5">
+        <AddButtonWithCounter v-model="cartItemCount" />
+      </v-col>
+      <v-col md="2" cols="3" class="d-flex justify-end">
+        <p class="primary--text mb-0 text-body-1 font-weight-medium">{{ productInfo.pricePerUnit }}</p>
+      </v-col>
+      <v-col md="2" cols="4" class="d-flex justify-end">
+        <p class="mr-4 my-0">Sil</p>
+      </v-col>
     </v-row>
   </v-card>
 </template>
 
 <script>
+import AddButtonWithCounter from '../input/AddButtonWithCounter.vue';
 export default {
-  data() {
-    return {
-      itemsInBasket: 1,
-    };
+  components: { AddButtonWithCounter },
+  props: { productID: { required: true }, productInfo: { default: () => ({}), required: true } },
+  computed: {
+    cartItemCount: {
+      get: function () {
+        return this.$store.getters['cart/items'][this.productID] || 0
+      },
+      set: function (value) {
+        this.$store.commit('cart/setCount', { id: this.productID, count: value })
+      }
+    },
   },
 };
 </script>
--- pages/my-cart.vue
+++ pages/my-cart.vue
@@ -12,12 +12,16 @@
             </v-row>
           </v-card-title>
           <v-divider class="mx-4"></v-divider>
-          <CartProductPreview />
-          <CartProductPreview />
+          <CartProductPreview
+            v-for="(item, id) in $store.getters['cart/items']"
+            :key="id"
+            :productID="id"
+            :productInfo="JSON.parse(cartItemInfos)[id]"
+          />
         </v-card>
       </v-col>
       <v-col cols="12" md="3">
-        <CartSummary />
+        <CartSummary :total-cart-price="totalPrice" />
       </v-col>
     </v-row>
   </v-container>
@@ -32,6 +36,46 @@ export default {
     CartProductPreview,
     CartSummary,
   },
+  data() {
+    return {
+      cartItemInfos: "{}",
+    }
+  },
+  methods: {
+    getCartProducts() {
+      this.$api("getCartProducts", Object.keys(this.$store.state['cart'].items)).then(({ data }) => {
+        let obj = {}
+        data.forEach(e => {
+          obj[e.productID] = e
+        });
+        this.cartItemInfos = JSON.stringify(obj)
+      })
+    }
+  },
+  computed: {
+    totalPrice() {
+      return Math.round(
+        Object.entries(this.$store.getters["cart/items"]).reduce(
+          (p, c) => {
+            return p + JSON.parse(this.cartItemInfos)[c[0]]?.pricePerUnit * c[1]
+          },
+          0
+        ) * 100
+      ) / 100
+    },
+    numberOfItemsInCart() {
+      return Object.keys(this.$store.getters["cart/items"]).length
+    }
+  },
+  watch: {
+    numberOfItemsInCart: function (p, c) {
+      if (p > c) this.getCartProducts()
+    }
+  },
+  mounted() {
+    if (this.numberOfItemsInCart > 0)
+      this.getCartProducts()
+  }
 };
 </script>
 
