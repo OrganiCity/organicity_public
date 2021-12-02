fa664f1a81cb79a0ee6eeb46b313dff241d08834
Ali Tarik <alitarik@sabanciuniv.edu>
2021-12-02 15:26:32 +0300

    first commit

---
+++ pages/seller/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <v-tabs v-model="tab">
+      <v-tab>My Products</v-tab>
+      <v-tab>Another Tab</v-tab>
+      <v-tab>Orders</v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        My Products
+      </v-tab-item>
+
+      <v-tab-item>
+        Another Tab
+      </v-tab-item>
+      <v-tab-item>
+        Orders   
+      </v-tab-item>
+    </v-tabs-items>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  components: {  },
+  layout:"admin",
+  data () {
+    return {
+      tab: 0,
+    };
+  },
+}
+</script>
+
+<style>
+
+</style>
