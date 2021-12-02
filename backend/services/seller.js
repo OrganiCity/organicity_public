ce6be051dca484991c6d88549811f8c600f0b148
Ali Tarik <alitarik@sabanciuniv.edu>
2021-12-02 16:00:53 +0300

    created backend

---
+++ backend/services/seller.js
@@ -0,0 +1,14 @@
+import { pool } from "../controller"
+import { isProvided } from "../utils"
+
+export function getStoreProducts(req, res) {
+  return res.status(200).send();
+  console.log("a")
+  if (!isProvided(req.body.userID)) return res.status(400).send("Bad request!")
+  const queryText = `SELECT * FROM products WHERE sellerID=?`;
+  const queryValues = [req.body.userID];
+  pool.query(queryText, queryValues, (err, data) => {
+          if (err) return res.status(500).send(err)
+          return res.status(200).send(data)
+      })
+}
