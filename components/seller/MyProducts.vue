96ca9d57274a03e304f3cc0bdac953ffe2326bc6
Ali Tarik <alitarik@sabanciuniv.edu>
2021-12-02 17:14:34 +0300

    added products

--- components/seller/MyProducts.vue
+++ components/seller/MyProducts.vue
@@ -1,22 +1,29 @@
 <template>
 <v-container>
-    <v-col v-for="product in products" :key="product.productID" cols="6" sm="4">
-      <ProductPreview :product-id="product.productID"/>
-    </v-col>
+  <v-row>
+    <v-btn class="ma-4" color="primary" large>Add New Product</v-btn>
+  </v-row>
+    <v-row>
+      <v-col v-for="product in products" :key="product.productID" cols="4" sm="3">
+        <SellerProductPreview :product-id="product.productID" />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import SellerProductPreview from "./SellerProductPreview.vue"
+
 export default {
+  components: { SellerProductPreview },
   data () {
     return {
-      products: [],
+      products: [{productID: 1}, {productID: 2}, {productID: 0}, {productID: 0}, {productID: 0}],
     }
   },
   mounted () {
-    this.$api("getStoreProducts", this.$store.getters["auth/userInfo"].userID).then((data)=>{
-      console.log(data);
-    })
+    // this.$api("getStoreProducts", this.$store.getters["auth/userInfo"].userID).then((data)=>{
+    // })
   }
 }
 </script>
