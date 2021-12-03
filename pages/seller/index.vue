ef17fcf1e82f1bd079e12801fdfad693f4eb6116
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-12-04 02:01:25 +0300

    expanded tabs added

--- pages/seller/index.vue
+++ pages/seller/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-tabs v-model="tab">
+    <v-tabs fixed-tabs v-model="tab">
       <v-tab>My Products</v-tab>
       <v-tab>Another Tab</v-tab>
       <v-tab>Orders</v-tab>
@@ -8,37 +8,35 @@
 
     <v-tabs-items v-model="tab">
       <v-tab-item>
-        <MyProducts :products="products"/>
+        <MyProducts :products="products" />
       </v-tab-item>
 
+      <v-tab-item>Another Tab</v-tab-item>
       <v-tab-item>
-        Another Tab
-      </v-tab-item>
-      <v-tab-item>
-        <Orders/>  
+        <Orders />
       </v-tab-item>
     </v-tabs-items>
   </v-container>
 </template>
 
 <script>
-import MyProducts from '~/components/seller/MyProducts.vue';
-import Orders from '~/components/seller/Orders.vue';
+import MyProducts from "~/components/seller/MyProducts.vue";
+import Orders from "~/components/seller/Orders.vue";
 
 export default {
-  components: {MyProducts, Orders  },
-  layout:"admin",
-  data () {
+  components: { MyProducts, Orders },
+  layout: "admin",
+  data() {
     return {
       tab: 0,
       products: [],
     };
   },
-   mounted () {
-     this.$api("getStoreProducts", {userID:6} ).then(({data})=>{
-       console.log(data)
-       this.products = data;
-   })
-  }
-}
+  mounted() {
+    this.$api("getStoreProducts", { userID: 6 }).then(({ data }) => {
+      console.log(data);
+      this.products = data;
+    });
+  },
+};
 </script>
