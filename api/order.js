a4b7d1cc165ebe751baed224485d5cf21c6e6b28
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-12-05 03:50:13 +0300

    my orders apis added

--- api/order.js
+++ api/order.js
@@ -1,4 +1,9 @@
 export async function getOrderPreviewsByID({ instance }, payload) {
     return await instance.get("/order/get-order-previews/" + payload)
   }
+
+  export async function getOrderDetailsByOrderNumber({ instance }, payload) {
+    return await instance.get("/order/get-order-details/" + payload)
+  }
+  
   
