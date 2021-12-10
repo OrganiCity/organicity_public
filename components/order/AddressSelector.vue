b0306d2f6af6d81d62176f4cdb93917b1fa2066e
Ali Tarik <alitarik@sabanciuniv.edu>
2021-12-10 16:17:12 +0300

    created components and page

---
+++ components/order/AddressSelector.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-col cols="12">
+    <v-radio-group v-model="selected">
+      <v-radio v-for="address in addresses" :key="address.name" :value="address.name">
+        <template v-slot:label>
+          <AddressSelectorCard :address="address"/>
+        </template>
+      </v-radio>
+      
+    </v-radio-group>
+  </v-col>
+</template>
+
+<script>
+import AddressSelectorCard from './AddressSelectorCard.vue';
+export default {
+  components: { AddressSelectorCard },
+  props: { addresses: [] },
+  data () {
+    return {
+      selected: 1,
+    }
+  }
+};
+</script>
+
+<style>
+</style>
