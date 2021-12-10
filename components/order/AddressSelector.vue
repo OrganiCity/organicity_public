2af29dca88da95331f8b9c329dc3911a25ad9df1
Ali Tarik <alitarik@sabanciuniv.edu>
2021-12-10 19:56:20 +0300

    fixed headers

--- components/order/AddressSelector.vue
+++ components/order/AddressSelector.vue
@@ -1,14 +1,11 @@
 <template>
-  <v-col cols="12">
-    <v-radio-group v-model="selected">
-      <v-radio v-for="address in addresses" :key="address.name" :value="address.name">
-        <template v-slot:label>
-          <AddressSelectorCard :address="address"/>
-        </template>
-      </v-radio>
-      
-    </v-radio-group>
-  </v-col>
+  <v-radio-group v-model="selected">
+    <v-radio v-for="address in addresses" :key="address.name" :value="address.name">
+      <template v-slot:label>
+        <AddressSelectorCard :address="address"/>
+      </template>
+    </v-radio>
+  </v-radio-group>
 </template>
 
 <script>
