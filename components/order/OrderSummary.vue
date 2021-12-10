b43b1cbbfb7184df641b700247e91f74bb0f1969
Ali Tarik <alitarik@sabanciuniv.edu>
2021-12-10 16:17:12 +0300

    created components and page

---
+++ components/order/OrderSummary.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="pa-4">
+    <p class="text-h5 ma-0">Sipariş Özeti</p>
+    <p class="text-body-2 mb-0">2 Ürün</p>
+    <v-divider class="my-4"></v-divider>
+    <div class="d-flex justify-space-between">
+      <span class="text-body-2">Sipariş Tutarı</span>
+      <span class="text-body-2 font-weight-medium">{{ totalCartPrice }} ₺</span>
+    </div>
+
+    <div class="d-flex justify-space-between">
+      <span class="text-body-2">Kargo Ücreti</span>
+      <span class="text-body-2 font-weight-medium">10,00 TL</span>
+    </div>
+
+    <div class="d-flex justify-space-between mt-6">
+      <span class="text-body-2 mb-0 font-weight-medium">Toplam Tutar</span>
+      <span class="text-body-2 primary--text font-weight-medium">{{ totalPrice }} ₺</span>
+    </div>
+
+    <div class="d-flex justify-space-between mb-4">
+      <span class="text-body-2 mb-0 font-weight-medium">Ödenecek Tutar</span>
+      <span class="text-body-2 primary--text font-weight-medium">{{ totalPrice }} ₺</span>
+    </div>
+
+    <v-btn elevation="0" color="primary" block>Sıradaki Adım</v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["total-cart-price"],
+  computed: {
+    totalPrice() {
+      return this.totalCartPrice + 10
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/complete-order.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container>
+    <v-row class="mt-5">
+      <v-col cols="9">
+        <v-row>
+          <v-col cols="12">
+            <p class="text-h4 mb-0">Teslimat Adresi</p>
+          </v-col>
+          <AddressSelector :addresses="addresses"/>
+        </v-row>
+        <v-row>
+          2 Ödeme Aracı
+        </v-row>
+        <v-row>
+          3 Ürünler ve Teslimat
+        </v-row>
+      </v-col>
+      <v-col cols="3">
+        <OrderSummary :total-cart-price="10" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AddressSelector from '~/components/order/AddressSelector.vue'
+import OrderSummary from '~/components/order/OrderSummary.vue'
+export default {
+  components: { AddressSelector, OrderSummary },
+  layout: "product",
+  data () {
+    return {
+      addresses:[
+        {
+          name: "Ev",
+          toName: "Doğkan",
+          toSurname: "Saraç",
+          country: "Turkey",
+          city: "İstanbul",
+          district: "Pendik",
+          ZIPorNeighbourhood: "Güllü Bağlar Mah.",
+          street: "Ulubat Sk.",
+          buildingAndFlatNo: "59/23",
+          description: "Büyük sarı bina",
+        },
+        {
+          name:"İş",
+          toName: "Doğkan",
+          toSurname: "Saraç",
+          country: "Turkey",
+          city: "İstanbul",
+          district: "Pendik",
+          ZIPorNeighbourhood: "Güllü Bağlar Mah.",
+          street: "Ulubat Sk.",
+          buildingAndFlatNo: "59/23",
+          description: "Büyük sarı bina",
+        },
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
