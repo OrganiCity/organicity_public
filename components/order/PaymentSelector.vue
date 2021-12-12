e4248d4e0b546fb70081b4090f39b9980d826e78
Ali Tarik <alitarik@sabanciuniv.edu>
2021-12-12 23:20:36 +0300

    added payment selection

---
+++ components/order/PaymentSelector.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-radio-group v-model="selected">
+    <v-radio>
+      <template v-slot:label>
+        Nakit
+      </template>
+    </v-radio>
+    <v-radio>
+      <template v-slot:label>
+        Kredi Kartı
+      </template>
+    </v-radio>
+  </v-radio-group>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selected: 1,
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- pages/complete-order.vue
+++ pages/complete-order.vue
@@ -2,23 +2,70 @@
   <v-container>
     <v-row class="mt-5">
       <v-col cols="9">
-        <v-row>
-          <v-col cols="12">
-            <p class="text-h4 mb-0">Teslimat Adresi</p>
-          </v-col>
-          <AddressSelector :addresses="addresses"/>
-        </v-row>
-        <v-row>
-          <v-col cols="12">
-            <p class="text-h4 mb-0">Ödeme Aracı</p>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12">
-            <p class="text-h4 mb-0">Ürünler ve Teslimat</p>
-          </v-col>
-        </v-row>
+        <v-stepper v-model="e1">
+          <v-stepper-header>
+            <v-stepper-step
+              :complete="e1 > 1"
+              step="1"
+            >
+              Teslimat Adresi
+            </v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step
+              :complete="e1 > 2"
+              step="2"
+            >
+              Ödeme Yöntemi
+            </v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step step="3">
+              Ürünler ve Teslimat
+            </v-stepper-step>
+          </v-stepper-header>
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <AddressSelector :addresses="addresses"/>
+              <v-btn
+                color="primary"
+                @click="e1 = 2"
+              >
+                Devam Et
+              </v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="2">
+              <PaymentSelector/>
+              <p class="font-weight-bold">Fatura Adresi</p>
+              <AddressSelector :addresses="addresses"/>
+              <v-btn
+                color="primary"
+                @click="e1 = 3"
+              >
+                Devam Et
+              </v-btn>
+              <v-btn text @click="e1 = 1">
+                Geri
+              </v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="3">
+              <v-card
+                class="mb-12"
+                color="grey lighten-1"
+                height="200px"
+              ></v-card>
+              <v-btn
+                color="primary"
+                @click="e1 = 1"
+              >
+                Siparişi Tamamla
+              </v-btn>
+              <v-btn text @click="e1 = 2">
+                Geri
+              </v-btn>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
       </v-col>
+
       <v-col cols="3">
         <OrderSummary :total-cart-price="10" />
       </v-col>
@@ -29,11 +76,13 @@
 <script>
 import AddressSelector from '~/components/order/AddressSelector.vue'
 import OrderSummary from '~/components/order/OrderSummary.vue'
+import PaymentSelector from '~/components/order/PaymentSelector.vue'
 export default {
-  components: { AddressSelector, OrderSummary },
+  components: { AddressSelector, OrderSummary, PaymentSelector },
   layout: "product",
   data () {
     return {
+      e1: 1,
       addresses:[
         {
           name: "Ev",
