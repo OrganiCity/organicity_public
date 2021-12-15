b61daf0d106f41b3270fe70c842d142f4e93e79a
Ali Tarik <alitarik@sabanciuniv.edu>
2021-12-15 15:39:05 +0300

    implemented in the page directly

--- components/order/ShipmentSelector.vue
+++ components/order/ShipmentSelector.vue
@@ -1,16 +1,5 @@
 <template>
-  <v-radio-group v-model="selected">
-    <v-radio>
-      <template v-slot:label>
-        Normal Kargo
-      </template>
-    </v-radio>
-    <v-radio>
-      <template v-slot:label>
-        Hızlı Kargo (+10₺)
-      </template>
-    </v-radio>
-  </v-radio-group>
+  
 </template>
 
 <script>
