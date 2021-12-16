cbf1ac1587075a34b7175fe2b53c000c7596e935
Ali Tarik <alitarik@sabanciuniv.edu>
2021-12-16 14:44:35 +0300

    added address addition, removal, update functions

--- backend/services/account.js
+++ backend/services/account.js
@@ -68,3 +68,61 @@ export function newSeller(req, res) {
         return res.status(200).send(data)
     });
 }
+
+export function getAddresses(req, res) {
+    const queryValues = [req.body.userID, req.body.type];
+    if (!isProvided(...queryValues)) return res.status(400).send("Invalid data!");
+    const queryText = `SELECT * FROM addresses WHERE userID = ? AND addressType = ?`
+
+    pool.query(queryText, queryValues, (err, data) => {
+        if(err) console.log(err);
+        if (err) return res.status(500).send("Internal Server Error")
+        return res.status(200).send(data)
+    });
+}
+
+export function removeAddress(req, res) {
+    console.log(req.body)
+    const queryValues = [req.body.ID];
+    if (!isProvided(...queryValues)) return res.status(400).send("Invalid data!");
+    const queryText = `DELETE FROM addresses WHERE ID = ?`
+
+
+    pool.query(queryText, queryValues, (err, data)=>{
+        return res.status(200).send();
+    })
+}
+
+export function updateAddress(req, res) {
+    
+    const queryValues = [req.body.phoneNumber, req.body.country, req.body.city, req.body.description, req.body.district, req.body.ZIPorNeighbourhood, req.body.name, req.body.street, req.body.toName, req.body.toSurname, req.body.buildingAndFlatNo, req.body.ID];
+
+    if (!isProvided(req.body.phoneNumber, req.body.country, req.body.city, req.body.description, req.body.name, req.body.toName, req.body.toSurname, req.body.ID)) return res.status(400).send("Invalid data!");
+    
+    
+    const queryText = `UPDATE addresses
+	    SET phoneNumber=?, country=?, city=?, description=?, district=?, ZIPorNeighbourhood=?, name=?, street=?, toName=?, toSurname=?, buildingAndFlatNo=?
+	    WHERE ID=?;`
+
+
+    pool.query(queryText, queryValues, (err, data)=>{
+        if(err) console.log(err)
+        if(err) res.status(500).send();
+        return res.status(200).send();
+    })
+}
+
+export function newAddress(req, res) {
+    const queryValues = [req.body.userID, req.body.addressType, req.body.phoneNumber, req.body.country, req.body.city, req.body.description, req.body.district, req.body.ZIPorNeighbourhood, req.body.name, req.body.street, req.body.toName, req.body.toSurname, req.body.buildingAndFlatNo];
+    console.log(queryValues)
+    if (!isProvided(req.body.userID, req.body.addressType, req.body.phoneNumber, req.body.country, req.body.city, req.body.description, req.body.name, req.body.toName, req.body.toSurname)) return res.status(400).send("Invalid data!");
+    const queryText = `INSERT INTO addresses
+    (userID, addressType, phoneNumber, country, city, description, district, ZIPorNeighbourhood, name, street, toName, toSurname, buildingAndFlatNo)
+    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?);`
+
+
+    pool.query(queryText, queryValues, (err, data)=>{
+        if(err) return res.status(500).send();
+        return res.status(200).send();
+    })
+}
--- pages/account/my-addresses.vue
+++ pages/account/my-addresses.vue
@@ -1,67 +1,74 @@
 <template>
-  <v-row class="my-4">
-    <v-col cols="12" class="d-flex justify-center">
-      <p class="text-h4 pa-0">Adreslerim</p>
-    </v-col>
+  <v-container class="pa-0 ma-0">  
+    <v-row class="my-4">
+      <v-col cols="12" class="d-flex justify-center">
+        <p class="text-h4 pa-0">Adreslerim</p>
+      </v-col>
 
-    <!-- Delivery Info -->
-    <v-col md="6" cols="12">
-      <p class="text-h6 mb-1" style="text-align: center">Teslimat Adreslerim</p>
-      <v-btn style="text-transform: none" block elevation="0" outlined color="primary">
-        <v-icon size="20px">add</v-icon>
-        Yeni Teslimat Adresi Ekle
-      </v-btn>
+      <!-- Delivery Info -->
+      <v-col md="6" cols="12">
+        <p class="text-h6 mb-1" style="text-align: center">Teslimat Adreslerim</p>
+        <v-btn style="text-transform: none" block elevation="0" outlined color="primary" @click="deliveryDialog=true">
+          <v-icon size="20px">add</v-icon>
+          Yeni Teslimat Adresi Ekle
+        </v-btn>
 
-      <AddressCard :address="address" />
-    </v-col>
+        <AddressCard v-for="address in deliveryAddresses" :key="address.ID" :address="address" @delete="loadAddresses"/>
+      </v-col>
 
-    <!-- Billing Info -->
-    <v-col md="6" cols="12">
-      <p class="text-h6 mb-1" style="text-align: center">Fatura Adreslerim</p>
-      <v-btn style="text-transform: none" block elevation="0" outlined color="primary">
-        <v-icon size="20px">add</v-icon>
-        Yeni Fatura Adresi Ekle
-      </v-btn>
-      <v-card outlined class="mt-6">
-        <v-card-title class="d-flex justify-space-between text-body-1 font-weight-bold pa-2 ma-0">
-          <span class="ma-2">ev</span>
-          <div>
-            <v-btn small class="pa-0 ma-0" icon>
-              <v-icon size="20px">mdi-pencil-outline</v-icon>
-            </v-btn>
-            <v-btn small class="pa-0 ma-0" icon>
-              <v-icon size="20px">delete_outline</v-icon>
-            </v-btn>
-          </div>
-          <p class="text-caption mx-2">Güllü Bağlar Mh. Ulubat Sk. Bina ve Daire No: No:59 Daire:23 Pendik/İstanbul</p>
-        </v-card-title>
-      </v-card>
-    </v-col>
-  </v-row>
+      <!-- Billing Info -->
+      <v-col md="6" cols="12">
+        <p class="text-h6 mb-1" style="text-align: center">Fatura Adreslerim</p>
+        <v-btn style="text-transform: none" block elevation="0" outlined color="primary" @click="billingDialog=true">
+          <v-icon size="20px">add</v-icon>
+          Yeni Fatura Adresi Ekle
+        </v-btn>
+
+        <AddressCard v-for="address in billingAddresses" :key="address.ID" :address="address" @delete="loadAddresses"/>
+      </v-col>
+    </v-row>
+    <v-dialog v-model="deliveryDialog" overlay-color="secondary" max-width="700px">
+      <AddressDialogMenu :address="deliveryTemplate" @close="deliveryDialog=false; loadAddresses();"/>
+    </v-dialog>
+    <v-dialog v-model="billingDialog" overlay-color="secondary" max-width="700px">
+      <AddressDialogMenu :address="billingTemplate" @close="billingDialog=false; loadAddresses();"/>
+    </v-dialog>
+  </v-container>
 </template>
 
 
 
 <script>
 import AddressCard from "~/components/account/AddressCard.vue";
+import AddressDialogMenu from "~/components/account/AddressDialogMenu.vue"
 export default {
   layout: "account",
   components: {
     AddressCard,
+    AddressDialogMenu,
   },
   data: () => ({
-    dialog: false,
-    address: {
-      toName: "Doğkan",
-      toSurname: "Saraç",
-      country: "Turkey",
-      city: "İstanbul",
-      district: "Pendik",
-      ZIPorNeighbourhood: "Güllü Bağlar Mah.",
-      street: "Ulubat Sk.",
-      buildingAndFlatNo: "59/23",
-      description: "Büyük sarı bina",
-    },
+    deliveryDialog: false,
+    billingDialog: false,
+    deliveryTemplate:{},
+    billingTemplate:{},
+    deliveryAddresses:[],
+    billingAddresses:[],
   }),
+  methods: {
+    loadAddresses() {
+      this.$api("getAddresses", {userID: this.$store.getters["auth/userInfo"].userID, type:'delivery'}).then(({ data }) => {
+        this.deliveryAddresses = data;
+      });
+      this.$api("getAddresses", {userID: this.$store.getters["auth/userInfo"].userID, type:'billing'}).then(({ data }) => {
+        this.billingAddresses = data;
+      });
+    }
+  },
+  mounted() {
+    this.loadAddresses();
+    this.deliveryTemplate = {userID: this.$store.getters["auth/userInfo"].userID, addressType:'delivery'};
+    this.billingTemplate = {userID: this.$store.getters["auth/userInfo"].userID, addressType:'billing'};
+  },
 };
 </script>
